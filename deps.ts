2fcc9224efc7dc0af2bb7f9b9c3c94ed37c6b98e
Roy Ivy III <rivy.dev@gmail.com>
2022-02-19 22:09:40 -0600

    change ~ specify format of ':age' and ':percent' more precisely

--- deps.ts
+++ deps.ts
@@ -1,2 +1,3 @@
 export { bgGreen, bgWhite } from 'https://deno.land/std@0.116.0/fmt/colors.ts';
 export { writeAllSync } from 'https://deno.land/std@0.116.0/io/util.ts';
+export { sprintf } from 'https://deno.land/std@0.126.0/fmt/printf.ts';
