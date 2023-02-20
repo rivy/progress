d1feb4de250ddbcfe870e29e6f5d3233cbee0d64
Roy Ivy III <rivy.dev@gmail.com>
2023-02-20 11:21:48 -0600

    docs ~ (examples) add show cursor on exit protection

--- examples/wifi-winos-netsh-wlan-show-interfaces.ts
+++ examples/wifi-winos-netsh-wlan-show-interfaces.ts
@@ -10,6 +10,7 @@
 // ref: <https://www.juniper.net/documentation/en_US/junos-space-apps/network-director3.7/topics/concept/wireless-ssid-bssid-essid.html> @@ <https://archive.is/gtmDY>
 
 import * as Colors from 'https://deno.land/std@0.126.0/fmt/colors.ts';
+import { writeAllSync } from 'https://deno.land/std@0.126.0/streams/conversion.ts';
 
 export const decoder = new TextDecoder(); // default == 'utf=8'
 // export const encoder = new TextEncoder(); // *always* 'utf-8'
@@ -18,6 +19,69 @@ export const decode = (input?: Uint8Array): string => decoder.decode(input);
 
 // import Progress from 'https://cdn.jsdelivr.net/gh/rivy/progress@1d0758f6f7/mod.ts';
 import Progress from './../mod.ts';
+
+//===
+
+// refs
+// * <https://stackoverflow.com/questions/62332153/deno-callback-on-exit>
+// * <https://deno.land/manual/examples/os_signals> @@ <https://archive.is/Ak1jc>
+// * <https://denolib.gitbook.io/guide/advanced/process-lifecycle> @@ <https://archive.is/8jDN6>
+// * [Clean up on `Deno.exit(...)`](https://github.com/denoland/deno/issues/3603)
+// * [Deno ~ OS Signals](https://deno.land/manual/examples/os_signals)
+// * [Deno ~ Program Lifecycle](https://deno.land/manual/runtime/program_lifecycle)
+// * [MDN ~ Event](https://developer.mozilla.org/en-US/docs/Web/API/Event)
+
+import * as $semver from 'https://deno.land/x/semver@v1.4.0/mod.ts';
+
+const isWinOS = Deno.build.os === 'windows';
+let exit_requested = false;
+
+/** Open a file specified by `path`, using `options`.
+ * * _`no-throw`_ function (returns `undefined` upon any error)
+ * @returns an instance of `Deno.FsFile`
+ */
+function denoOpenSyncNT(path: string | URL, options?: Deno.OpenOptions) {
+	// no-throw `Deno.openSync(..)`
+	try {
+		return Deno.openSync(path, options);
+	} catch {
+		return undefined;
+	}
+}
+
+// restore cursor display on console (regardless of process exit path)
+const ansiCSI = { showCursor: '\x1b[?25h', hideCursor: '\x1b[?25l', clearEOL: '\x1b[0K' };
+['unload'].forEach((eventType) =>
+	addEventListener(eventType, (_: Event) => {
+		// ref: https://unix.stackexchange.com/questions/60641/linux-difference-between-dev-console-dev-tty-and-dev-tty0
+		const consoleFileName = isWinOS ? 'CONOUT$' : '/dev/tty';
+		const file = denoOpenSyncNT(consoleFileName, { write: true });
+		if (file != null) {
+			writeAllSync(file, (new TextEncoder()).encode(ansiCSI.showCursor));
+			Deno.close(file.rid);
+		}
+	})
+);
+
+try {
+	// catch SIGBREAK and SIGINT to avoid abrupt process exits
+	// * use `exit_requested` as an orderly exit signal to the main application loop
+	// * note: for success, requires Deno v1.23.0+ (ref: <https://github.com/denoland/deno/pull/14694> , <https://github.com/denoland/deno/releases/tag/v1.23.0>)
+	const s: Deno.Signal[] = (isWinOS && ($semver
+			.satisfies(Deno.version.deno, '>=1.23.0'))
+		? ['SIGBREAK'] as Deno.Signal[]
+		: [])
+		.concat(['SIGINT']);
+	s.forEach((signalType) =>
+		Deno.addSignalListener(signalType, () => {
+			exit_requested = true;
+		})
+	);
+} catch (_e) {
+	// console.warn('Caught exception...', { _e });
+}
+
+//===
 
 function netshWlanShowInterfaces() {
 	try {
@@ -101,46 +165,16 @@ function qualityLevelInfo(dBm: number) {
 	return qualityLevels.find((e) => dBm >= e.dBm) ?? qualityLevels[qualityLevels.length - 1];
 }
 
-// refs
-// * <https://stackoverflow.com/questions/62332153/deno-callback-on-exit>
-// * <https://deno.land/manual/examples/os_signals> @@ <https://archive.is/Ak1jc>
-// * <https://denolib.gitbook.io/guide/advanced/process-lifecycle> @@ <https://archive.is/8jDN6>
-// * [Clean up on `Deno.exit(...)`](https://github.com/denoland/deno/issues/3603)
-// * [Deno ~ OS Signals](https://deno.land/manual/examples/os_signals)
-// * [Deno ~ Program Lifecycle](https://deno.land/manual/runtime/program_lifecycle)
-// * [MDN ~ Event](https://developer.mozilla.org/en-US/docs/Web/API/Event)
-
-import * as $semver from 'https://deno.land/x/semver@v1.4.0/mod.ts';
-
-const isWinOS = Deno.build.os === 'windows';
-let exit_requested = false;
-
-try {
-	const s: Deno.Signal[] = (isWinOS && ($semver
-			.satisfies(Deno.version.deno, '>=1.23.0'))
-		? ['SIGBREAK'] as Deno.Signal[]
-		: [])
-		.concat(['SIGINT']);
-	// console.warn('Listen for %s signals', JSON.stringify(s));
-	s.forEach((signalType) =>
-		Deno.addSignalListener(signalType, () => {
-			exit_requested = true;
-		})
-	);
-} catch (_e) {
-	// console.warn('Caught exception...', { _e });
-}
-
 // console.warn('Script is initialized and starting...');
 
 const nReadings = /* 10 */ Infinity;
-const arr: Map<string, string>[] = [];
-const goal = 100;
+// const arrayForWhat: Map<string, string>[] = [];
+
 const progress = new Progress({
 	title: 'WiFi Signals',
-	goal,
+	// completeTemplate: 'done',
 	autoComplete: false,
-	// hideCursor: true,
+	hideCursor: true,
 });
 
 // progress.log('WiFi Signals (via `log()`)');
@@ -148,12 +182,14 @@ const progress = new Progress({
 // ref: [Infinite loops and SIGINT (aka, "don't block the JS event loop")](https://stackoverflow.com/questions/22594723/how-does-catching-ctrl-c-works-in-node) @@ <https://archive.is/BZRKM>
 // ref: [NodeJS ~ SIGINT and loops](https://github.com/nodejs/node/issues/9050)
 let index = 0;
+// * debounce occasional signal aquisition failure
+const maxSequentialFailures = 3;
+let sequentialFailures = 0;
 const _ = setInterval(async function () {
 	// for (let index = 0; !exit_requested && (index < nReadings); index++) {
 	index = (index < Number.MAX_SAFE_INTEGER) ? index + 1 : index;
 	if (index > nReadings) exit_requested = true;
 	// console.warn({ index, exit_requested });
-	// ToDO: debounce signalQuality results
 	const { signalQuality, wifiInterfaceData } = exit_requested
 		? { signalQuality: NaN, wifiInterfaceData: undefined }
 		: await (async () => {
@@ -162,10 +198,14 @@ const _ = setInterval(async function () {
 				?.filter((e) => e.has('Name') && e.has('BSSID'))
 				.map((e) => e.set('@', new Date().toISOString()));
 			// console.warn({ wifiInterfaceData });
-			wifiInterfaceData?.forEach((e) => arr.push(e));
+			// wifiInterfaceData?.forEach((e) => arrayForWhat.push(e));
 			const signalQuality = Number(wifiInterfaceData?.[0]?.get('Signal')?.match(/\d+/));
 			return { signalQuality, wifiInterfaceData };
 		})();
+	if (Number.isNaN(signalQuality)) {
+		sequentialFailures += 1;
+		if (sequentialFailures > maxSequentialFailures) exit_requested = true;
+	} else sequentialFailures = 0;
 	if (!exit_requested && !Number.isNaN(signalQuality)) {
 		const dBm = dBmFromQuality(signalQuality);
 		const qualityLevel = qualityLevelInfo(dBm);
@@ -179,7 +219,8 @@ const _ = setInterval(async function () {
 			barSymbolIncomplete: qualityLevel.background,
 			progressTemplate: `${prefix} * {percent} * {bar} *`,
 		});
-	} else {
+	}
+	if (exit_requested) {
 		progress.complete();
 		Deno.exit(0);
 	}
