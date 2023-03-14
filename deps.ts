6a0595ff3db3802cd92e5c3a000df2c603c794c1
Roy Ivy III <rivy.dev@gmail.com>
2023-03-14 00:43:30 -0500

    deps ~ add cli-spinners, GraphemeSplitter, and stringWidth

--- deps.ts
+++ deps.ts
@@ -1,3 +1,7 @@
 export { bgGreen, bgWhite } from 'https://deno.land/std@0.126.0/fmt/colors.ts';
 export { sprintf } from 'https://deno.land/std@0.126.0/fmt/printf.ts';
 export { writeAllSync } from 'https://deno.land/std@0.126.0/streams/conversion.ts';
+
+export { default as cliSpinners } from 'https://esm.sh/cli-spinners@2.7.0';
+// export { default as GraphemeSplitter } from 'https://esm.sh/grapheme-splitter@1.0.4';
+export { default as stringWidth } from 'https://esm.sh/string-width@5.1.2';
